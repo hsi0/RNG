320fdfc911b68b93f4499f8404bb5337595dcbd5
hsi0 <haiduc.s@yahoo.com>
2018-04-12 15:45:46 +0300

    Initial commit

---
+++ src/rng.java
@@ -0,0 +1,17 @@
+
+public class rng {
+	
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long rng;
+		long minnumber=1;
+		long maxnumber=1000;
+		
+	rng=minnumber+(long)(Math.random()*(maxnumber-minnumber)+1);
+		System.out.print(rng);
+	
+	}
+
+}
